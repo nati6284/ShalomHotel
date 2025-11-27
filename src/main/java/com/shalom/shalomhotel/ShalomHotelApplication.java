3c0b46ad802703ec1a80c4a3d795fba9dd70b125
nati6284 <natnaelabera6284@gmail.com>
2025-11-27 16:37:32 +0300

    init commit

---
+++ src/main/java/com/shalom/shalomhotel/ShalomHotelApplication.java
@@ -0,0 +1,13 @@
+package com.shalom.shalomhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShalomHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShalomHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shalom/shalomhotel/ShalomHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalom.shalomhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShalomHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
